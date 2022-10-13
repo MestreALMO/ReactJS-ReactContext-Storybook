f08036e39b35dcdb1bfdeebfff0d3c47f1c24fd8
André Lusegardis <lusegardis@gmail.com>
2022-10-13 15:52:53 -0300

    implemented a simple counter that uses ReactContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useCount } from "./context/Count";
+
 function App() {
+  const { count, setCount } = useCount();
+
   return (
     <>
       <h1>DevALMO, YouTube Channel</h1>
+
+      <h2>{count}</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +1
+      </button>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import CountProvider from "./context/Count";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <CssBaseline />
+    <CountProvider>
+      <App />
+      <CssBaseline />
+    </CountProvider>
   </React.StrictMode>
 );
